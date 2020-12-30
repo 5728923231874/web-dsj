5e7881aa8c45e9dd0dfd3e141edf4d9da81f3e23
Big <572892323@qq.con>
2020-12-30 17:10:19 +0800

    完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -12,7 +12,7 @@ $.ajaxPrefilter(function(options) {
   }
   
   // 不论成功还是失败，最终都会调用 complete 回调函数
-  options.complete = function (res) {
+  options.complete =function (res) {
     // console.log('执行了 complete 回调：')
     // console.log(res)
     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
--- assets/js/login.js
+++ assets/js/login.js
@@ -36,7 +36,7 @@ $('#form_reg').on('submit', function(e) {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
+    $.post('/api/reguser', data, function(res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
         //   console.log(res.message || '注册失败');
@@ -54,7 +54,7 @@ $('#form_login').submit(function(e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
-      url: 'http://ajax.frontend.itheima.net/api/login',
+      url: '/api/login',
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
